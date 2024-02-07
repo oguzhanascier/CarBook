e41c6eea5195e8f89f5286a4f730056a7302ea26
oguzhanascier <oguzhanascier20@gmail.com>
2024-02-08 01:27:25 +0300

    aa

---
+++ Core/CarBook.Domain/Entities/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Brand
+    {
+        public int BrandId { get;}
+        public string BrandName { get;}
+        public List<Car> Cars { get;}
+
+    }
+}
